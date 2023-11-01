bd750877479a2625f2af2385f32d8f7a65567115
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-11-01 11:44:12 +0300

    Update find_elem_max.cpp

--- file/find_elem_max.cpp
+++ file/find_elem_max.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 void read_info(vector<int> &A, int &n) {
 	freopen("input.txt", "rt", stdin);
-	cin >> n;
 
 	int elem;
 	for (int i = 0; i < n; i++) {
@@ -22,7 +21,7 @@ void read_info(vector<int> &A, int &n) {
 }
 
 void write_info(int max1, int max2, int max3, int max4, int max5) {
-	freopen("outout.txt", "wt", stdout);
+	freopen("output/output.txt", "wt", stdout);
 	cout << max1 << " ";
 	cout << max2 << " ";
 	cout << max3 << " ";
@@ -38,6 +37,7 @@ void solve(vector<int> &A) {
 
 int main() {
 	int n;
+	cin >> n;
 	vector<int> A(n);
 	read_info(A, n);
 	solve(A);
