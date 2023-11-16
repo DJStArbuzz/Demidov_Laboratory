cb5be7ea325b2d589478eadaef59700162a22bec
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-11-16 18:32:01 +0300

    Update 1_17.cpp

--- lab_work/order/2lab/1_17.cpp
+++ lab_work/order/2lab/1_17.cpp
@@ -3,20 +3,25 @@
 
 using namespace std;
 
+// Процедура ввода массива в консоль
 void cin_for_array(int* array, int n) {
 	for (int i = 0; i < n; i++) {
 		cin >> array[i];
 	}
 }
 
+// Процедура вывода массива в консоль
 void cout_for_array(int* array, int n) {
 	for (int i = 0; i < n; i++) {
 		cout << array[i] << " ";
 	}
 }
 
+// Процедура редактирования массива
 void editing_array(int* arrayA, int* arrayB, int n) {
 	for (int i = 0; i < n; i++) {
+		// если ai > 0, то bi увеличить в 10 раз,
+		// иначе bi заменить нулем.
 		if (arrayA[i] > 0) {
 			arrayB[i] *= 10;
 		}
@@ -30,9 +35,13 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	/* Задание 1, вариант 17
 	Даны натуральное n, целые числа a1, . . . , an и b1, . . . , bn.
-	Преобразовать последовательность bn по правилу: 
-	если ai > 0, то bi увеличить в 10 раз, 
+	Преобразовать последовательность bn по правилу:
+	если ai > 0, то bi увеличить в 10 раз,
 	иначе bi заменить нулем.
+	
+	A:  1 2 -2 -3 2
+	B:  10 2 3 2 1
+	B:  100 20 0 0 10
 	*/
 
 	int n;           // Количество элементов
