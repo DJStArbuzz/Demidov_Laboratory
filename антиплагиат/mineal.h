fe11215801537c3aec1dd889e15b01850ee26a9e
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-03-20 11:10:00 +0300

    Update mineal.h

--- антиплагиат/mineal.h
+++ антиплагиат/mineal.h
@@ -1,10 +1,13 @@
 #pragma once
+#include <string>
+#include <vector>
 #include "had.h"
+
 
 void Result_all_stud(
 	string Name_student,
-	vector<Solution_stud> &sol_stud,
-	vector<pair<string, int>> &All_stud) 
+	vector<Solution_stud>& sol_stud,
+	vector<pair<string, int>>& All_stud)
 {
 	pair<string, int> info_one_stud;
 	info_one_stud.first = Name_student;
@@ -42,5 +45,47 @@ string Generate_path_coach(string Name_number_contest,
 	path += Name_number_contest + '/';
 	path += Name_tasks + ".txt";
 
-		return path;
+	return path;
+}
+
+int Time_solution(string Data) {
+	string day = "";
+	day += Data[0] + Data[1];
+
+	string month;
+	months += Data[3] + Data[4];
+	
+	string year = "";
+	year += Data[6] + Data[7] + Data[8] + Data[9];
+	
+	string clock = "";
+	clock += Data[11] + Data[12];
+
+	string minute = "";
+	minute += Data[14] + Data[15];
+
+	int time = (stoi(year) * 365 + stoi(month) * 31 + stoi(day));
+	time = (time * 24 + stoi(clock)) * 60 + stoi(minute);
+	return time;
+}
+
+void check_time_solution(vector<Solution_stud> &sol_stud) 
+{
+	for (int i = 0; i < sol_stud.size(); i++) {
+		if (sol_stud[i].verdict != "OK") {
+			int raz = 1e5;
+			for (int j = 0; j < sol_stud.size(); j++) {
+				if (i != j) {
+					if (raz > abs(sol_stud[i].Time - sol_stud[j].Time) 
+					{
+						raz = abs(sol_stud[i].Time - sol_stud[j].Time);
+					}
+				}
+			}
+		}
+
+		if (raz <= 3) {
+			sol_stud[i].verdict = "Time";
+		}
+	}
 }
