3d544ab8aa8c106e9b9a1746d5abebf7405ee96e
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-03-26 22:06:36 +0300

    Update lab_7_3prob.cpp

--- lab_work/lab_7/lab_7_3prob.cpp
+++ lab_work/lab_7/lab_7_3prob.cpp
@@ -3,151 +3,66 @@
 #include <map>
 #include <random>
 #include <algorithm>
+#include <cmath>
 #include <random>
 #include "glut.h"
 
-const int width = 500;
-const int height = 500;
-
-const int N = 3;
-const int M = 13;
-const int K = 1000000;
-const double R = 0.5;
-
 using namespace std;
 
-struct Point {
-    double x = 0.0, y = 0.0;
 
-    Point operator+(Point B) {
-        Point res;
-        res.x = x + B.x;
-        res.y = y + B.y;
-        return res;
-    }
-
-    Point operator-(Point B) {
-        Point res;
-        res.x = x - B.x;
-        res.y = y - B.y;
-        return res;
-    }
-
-    Point operator*(double k) {
-        Point res;
-        res.x = x * k;
-        res.y = y * k;
-        return res;
-    }
-
-    Point operator/(double k) {
-        Point res;
-        res.x = x / k;
-        res.y = y / k;
-        return res;
-    }
-};
-
-void init2D(float r, float g, float b)
-{
-	glClearColor(r, g, b, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0, 500.0, 0, 500.0);
+bool check(double x, double y) {
+    return (pow(x, 7) + 2 * pow(y, 3) < 1) && (x + y > -1)
+        && (-2 < x < 2) && (-2 < y < 2);
 }
 
-
-int draw() {
-    int result = rand() % M;
-    return result % N;
+void information_problem3(int k, int N, int S0) {
+    cout << "Дана фигура, заданная системой уравнений.\n";
+    cout << "/ x^7 + 2 * y^3 < 1\n";
+    cout << "| x + y > -1\n";
+    cout << "| -2 < x < 2\n";
+    cout << "\ -2 < y < 2\n";
+    cout << "Площадь методом Монте-Карло: K * S | N \n";
+    cout << "k  = " << k << "\n";
+    cout << "N  = " << N << "\n";
+    cout << "S0 = " << S0 << "\n";
+    cout << "S  = " << 1.0 * k * S0 / N << "\n";
 }
 
-void Generate_points(vector<Point>& A, int n) {
-    glPointSize(5.0);
-    glColor3f(1, 0, 0);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < n; i++) {
-        A[i].x = abs((rand() % width) - width / 2);
-        A[i].y = abs((rand() % height) - height / 2);
-        glVertex2d(A[i].x, A[i].y);
-    }
-    glEnd();
-}
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
 
-void display(void)
-{
+    pair<double, double> f, s;
+    f.first = -2.0, f.second = -2.0;
+    s.first = 2.0, s.second = 2.0;
 
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(3.0, 0.0, 0.0);
-
-    vector<Point> list(N);
-    Generate_points(list, N);
-
-	glPointSize(6.0);
-	glBegin(GL_POINTS);
-	
-    Point Start;
-    Start.x = (rand() % width) - width / 2;
-    Start.y = (rand() % height) - height / 2;
-
-    for (int i = 1; i < K; i++) {
-        int it = draw();
-        
-        Point B = Start + list[it];
-        B = B * 0.5;
-
-        glPointSize(2.0);
-        glColor3f(0, 1, 0);
-        glBegin(GL_POINTS);
-        glVertex2d(B.x, B.y);
-        glEnd();
-        Start = B;
-
-    }
-    glutSwapBuffers();
-    glFlush();
-	
-}
-
-int main(int argc, char* argv[])
-{
-	setlocale(LC_ALL, "Russian");
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(width, height);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Задание 2, вариант 30");
-	init2D(0.0, 0.0, 0.0);
-	glutDisplayFunc(display);
-	glutMainLoop();
-
-	return 0;
-}
-
-
-
-/*
-using namespace std;
-
-int main() {
-    int n = 20;
-
-    int up = 2;
-    int nextBeg = 2;
-    int dist = 1;
-
-    cout << 1 << '\n';
-    for (int i = 2; i <= n; i++) {
-        if (i == nextBeg) {
-            cout << '\n';
-            dist++;
-            nextBeg += up;
-            up++;
+    int k = 0, N = 0, S = (abs(s.first) + abs(f.first)) * (abs(s.second) + abs(f.second));
+    for (float x = min(s.first, f.first); x <= max(s.first, f.first); x += 0.005) {
+        for (float y = min(s.second, f.second); y <= max(s.second, f.second); y += 0.005) {
+            if (check(x, y)) {
+                glVertex2f(x / 2, y / 2);
+                k++;
+            }
+            N++;
         }
-        cout << i << ' ';
     }
 
-    cout << "\n\n\n";
-    cout << dist;
+    information_problem3(k, N, S);
+    glEnd();
+    glFlush();
 }
-*/
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutCreateWindow("Задание 3, вариант 19");
+    glutDisplayFunc(display);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glutMainLoop();
+    return 0;
+}
