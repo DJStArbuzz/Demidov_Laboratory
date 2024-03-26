50aeed172a1584f349b551b9d6dc91c388d7b478
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-03-26 22:05:26 +0300

    Update lab_7_2prob.cpp

--- lab_work/lab_7/lab_7_2prob.cpp
+++ lab_work/lab_7/lab_7_2prob.cpp
@@ -6,79 +6,71 @@
 #include <random>
 #include "glut.h"
 
-
 const int width = 500;
 const int height = 500;
 
-const int N = 4;
+const int N = 3;
 const int M = 13;
 const int K = 1000000;
 const double R = 0.5;
 
 using namespace std;
 
-typedef pair<double, double> pdd;
-
 struct Point {
-	double x, y; // Координаты точки
+    double x = 0.0, y = 0.0;
 
-	Point(double xTmp, double yTmp);
+    Point operator+(Point B) {
+        Point res;
+        res.x = x + B.x;
+        res.y = y + B.y;
+        return res;
+    }
+
+    Point operator-(Point B) {
+        Point res;
+        res.x = x - B.x;
+        res.y = y - B.y;
+        return res;
+    }
+
+    Point operator*(double k) {
+        Point res;
+        res.x = x * k;
+        res.y = y * k;
+        return res;
+    }
+
+    Point operator/(double k) {
+        Point res;
+        res.x = x / k;
+        res.y = y / k;
+        return res;
+    }
 };
-Point::Point(double xTmp, double yTmp) :
-	x(xTmp),
-	y(yTmp) {}
-
 
 void init2D(float r, float g, float b)
 {
 	glClearColor(r, g, b, 0.0);
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+	gluOrtho2D(0, 500.0, 0, 500.0);
 }
 
-map <int, vector<int>> createTravelPos() {
-	map<int, vector<int>> res = {
-		{1, {}}, {2, {}}, {3, {}}, {4, {}}
-	};
 
-	vector<int> list(M);
-	for (int i = 0; i < M; i++) {
-		list[i] = i;
-	}
-
-	random_device rd;
-	mt19937 g(rd());
-	shuffle(list.begin(), list.end(), g);
-
-	int index = 0;
-	for (int i = 0; i < M; ++i) {
-		res[(i % N) + 1].push_back(list[i] + 1);
-	}
-
-	for (int i = 1; i <= N; i++) {
-		cout << "\nТочка " << i << ": ";
-		for (int j = 0; j < res[i].size(); j++) {
-			cout << res[i][j] << " ";
-		}
-	}
-	cout << '\n';
-	return res;
+int draw() {
+    int result = rand() % M;
+    return result % N;
 }
 
-double distance(Point first, Point second) {
-	return sqrt(pow(second.x - first.x, 2) + pow(second.y - first.y, 2));
-}
-
-int negaFlag(double first, double second) {
-	if (first < second) {
-		return 1;
-	}
-	else if (first > second) {
-		return -1;
-	}
-	else {
-		return 0;
-	}
+void Generate_points(vector<Point>& A, int n) {
+    glPointSize(5.0);
+    glColor3f(1, 0, 0);
+    glBegin(GL_POINTS);
+    for (int i = 0; i < n; i++) {
+        A[i].x = abs((rand() % width) - width / 2);
+        A[i].y = abs((rand() % height) - height / 2);
+        glVertex2d(A[i].x, A[i].y);
+    }
+    glEnd();
 }
 
 void display(void)
@@ -88,158 +80,37 @@ void display(void)
 
 	glColor3f(3.0, 0.0, 0.0);
 
-	vector<Point> list = {
-		Point(25.0, 25.0),
-		Point(60.0, 25.0),
-		Point(150.0, 100.0),
-		Point(50.0, 150.0)
-	};
+    vector<Point> list(N);
+    Generate_points(list, N);
 
 	glPointSize(6.0);
 	glBegin(GL_POINTS);
-	for (int j = 0; j < list.size(); j++) {
-		glVertex2i(list[j].x, list[j].y);
-	}
-	glEnd();
+	
+    Point Start;
+    Start.x = (rand() % width) - width / 2;
+    Start.y = (rand() % height) - height / 2;
 
-	map<int, vector<int>> arrays = createTravelPos();
-	vector<Point> newList = { 
-		Point(25.0, 25.0),
-		Point(60.0, 25.0),
-		Point(150.0, 100.0),
-		Point(50.0, 150.0) };
+    for (int i = 1; i < K; i++) {
+        int it = draw();
+        
+        Point B = Start + list[it];
+        B = B * 0.5;
 
-	int K = 10000;
-	for (int i = 1; i < M; i++)
-	{
-		cout << i << " этап:\n";
-		int posFirst = 0, posSecond = 0;
+        glPointSize(2.0);
+        glColor3f(0, 1, 0);
+        glBegin(GL_POINTS);
+        glVertex2d(B.x, B.y);
+        glEnd();
+        Start = B;
 
-		for (int j = 1; j <= N; j++) {
-			if (count(arrays[j].begin(), arrays[j].end(), i) == 1)
-			{
-				posFirst = j - 1;
-				break;
-			}
-		}
-		for (int j = 1; j <= N; j++) {
-			if (count(arrays[j].begin(), arrays[j].end(), i + 1) == 1) {
-				posSecond = j - 1;
-				break;
-			}
-		}
-		if (posFirst == posSecond) {
-			continue;
-		}
-
-		Point first = list[posFirst], second = list[posSecond];
-		/*glBegin(GL_LINES);
-		glVertex2i(first.x, first.y);
-		glVertex2i(second.x, second.y);
-		glEnd();*/
-		double dist = distance(first, second);
-		cout << "a: " << first.x << " " << first.y << " " << posFirst << endl;
-		cout << "b: " << second.x << " " << second.y << " " << posSecond << endl;
-		cout << dist << " расстояние" << endl;
-		int negaFlagX = negaFlag(first.x, second.x);
-		int negaFlagY = negaFlag(first.y, second.y);
-		glColor3f(0.0, 1.0, 0.0);
-		Point tmpFirst(first.x, first.y);
-		while (dist >= 0.005) {
-			double dx = abs(second.x - tmpFirst.x) / (pow(R, -1)) * negaFlagX;
-			double dy = abs(second.y - tmpFirst.y) / (pow(R, -1)) * negaFlagY;
-			glBegin(GL_POINTS);
-			glPointSize(6.0);
-			for (int j = 1; j <= int(pow(R, -1)) - 1; j++)
-			{
-				glVertex2i(tmpFirst.x + dx * j, tmpFirst.y + dy * j);
-				cout << tmpFirst.x + dx * j << " " << tmpFirst.y + dy * j << endl;
-				K--;
-				newList.push_back(Point(tmpFirst.x + dx * j, tmpFirst.y + dy * j));
-			}
-
-			glEnd();
-
-			tmpFirst.x += dx * (int(pow(R, -1)) - 1);
-			tmpFirst.y += dy * (int(pow(R, -1)) - 1);
-			dist = distance(tmpFirst, second);
-		}
-
-		cout << "конец этапа\n\n";
-	}
-
-	while (K > 0) {
-		if (K < 0) {
-			break;
-		}
-		vector<Point> newList2;
-		for (int i = 0; i < newList.size(); i++) {
-			for (int l = i + 1; l < newList.size(); l++) {
-				Point first = newList[i], second = newList[l];
-				/*
-				glBegin(GL_LINES);
-				glVertex2i(first.x, first.y);
-				glVertex2i(second.x, second.y);
-				glEnd();
-				*/
-				double dist = distance(first, second);
-
-				int negaFlagX = negaFlag(first.x, second.x);
-				int negaFlagY = negaFlag(first.y, second.y);
-
-				while (dist >= 0.005) {
-					double dx = abs(second.x - first.x) / (pow(R, -1)) * negaFlagX;
-					double dy = abs(second.y - first.y) / (pow(R, -1)) * negaFlagY;
-
-					glBegin(GL_POINTS);
-					glPointSize(6.0);
-					for (int r = 1; r <= int(pow(R, -1)) - 1; r++)
-					{
-						glVertex2i(first.x + dx * r, first.y + dy * r);
-						cout << first.x + dx * r << " " << first.y + dy * r << endl;
-						K -= 1;
-						if (K < 0) {
-							break;
-						}
-						cout << K <<" " << newList.size() <<  endl << endl;
-						newList2.push_back(Point(first.x + dx * r, first.y + dy * r));
-					}
-
-					glEnd();
-
-					first.x += dx * (int(pow(R, -1)) - 1);
-					first.y += dy * (int(pow(R, -1)) - 1);
-					dist = distance(first, second);
-				}
-				if (K < 0) {
-					break;
-				}
-			}
-			if (K < 0) {
-				break;
-			}
-		}
-
-
-		for (int i = 0; i < newList2.size(); i++) {
-			newList.push_back(newList2[i]);
-		}
-
-	}
-
-	cout << " " << newList.size() << endl;
-	for (int i = 0; i < newList.size(); i++) {
-		cout << newList[i].x << " " << newList[i].y << endl;
-	}
-	glFlush();
+    }
+    glutSwapBuffers();
+    glFlush();
+	
 }
 
 int main(int argc, char* argv[])
 {
-	/*
-	Построить "ломанную" по точкам. Точек = N, Операций = M - 1.
-	Коэффициент подобия = 0.5
-	*/
 	setlocale(LC_ALL, "Russian");
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -252,3 +123,31 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+
+
+/*
+using namespace std;
+
+int main() {
+    int n = 20;
+
+    int up = 2;
+    int nextBeg = 2;
+    int dist = 1;
+
+    cout << 1 << '\n';
+    for (int i = 2; i <= n; i++) {
+        if (i == nextBeg) {
+            cout << '\n';
+            dist++;
+            nextBeg += up;
+            up++;
+        }
+        cout << i << ' ';
+    }
+
+    cout << "\n\n\n";
+    cout << dist;
+}
+*/
