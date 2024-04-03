27b74612ff21366d0eaee49c0d452b76ad71e253
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-04-03 22:03:28 +0300

    Update lab_7_5prob.cpp

--- lab_work/lab_7/lab_7_5prob.cpp
+++ lab_work/lab_7/lab_7_5prob.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <random>
 #include <sstream>
+#include <queue>
+#include <set>
 #include "glut.h"
 
 const int WIDTH = 1000;
@@ -14,18 +16,31 @@ const double R = 0.5;
 
 using namespace std;
 
-vector<vector<int>> moves =
+vector<vector<int>> movesHorse =
 {    {-3, -1, -1},
     {2, -1, -1},
     {3, 1, 1},
     {-2, 1, 1},
-    { - 2, -2, -2},
-    {0, -2, -2},
-    {0, 2, 2},
-    {2, 2, 2},
 
+    { - 3, -2, -2},
+    {1, -2, -2},
+    {-1, 2, 2},
+    {3, 2, 2},
+
+    { -1, -3, -3},
+    {-2, -3, -3},
+    {1, 3, 3},
+    {2, 3, 3},
 };
 
+vector<vector<int>> movesKing =
+{ {0, 1, 1},
+    {1, 1, 1},
+    {-1, 0, 0},
+    {1, 0, 0},
+    {0, -1, -1},
+    {-1, -1, -1},
+};
 
 struct Point {
     double x = 0.0;
@@ -104,15 +119,10 @@ bool isValid(int x, int y, int dist) {
 void horse(Point tmp, vector<Point> &list) {
 
     int knightX = tmp.col, knightY = tmp.row; // координаты коня
-    for (auto move1 : moves)
+    for (auto move1 : movesHorse)
     {
         int nextY = knightY + move1[1];
         int nextX = knightX;
-        if (((nextX == tmp.dist + move1[2] - 1) || nextX == 0) && (abs(move1[2]) == 2)) {
-            continue;
-        }
-
-
 
         nextX += move1[0];
         if (isValid(nextX, nextY, tmp.dist + move1[2])) {
@@ -128,6 +138,112 @@ void horse(Point tmp, vector<Point> &list) {
         }
     }
 }
+
+void king(Point tmp, vector<Point>& list) {
+
+    int knightX = tmp.col, knightY = tmp.row; // координаты коня
+    for (auto move1 : movesKing)
+    {
+        int nextY = knightY + move1[1];
+        int nextX = knightX;
+
+        nextX += move1[0];
+        if (isValid(nextX, nextY, tmp.dist + move1[2])) {
+            cout << "(" << nextX << "," << nextY << ")" << endl;
+            for (int i = 0; i < list.size(); i++)
+            {
+                if (list[i].row == nextY && list[i].col == nextX) {
+                    renderText(list[i].x, list[i].y, "XYI");
+                    cout << list[i].index << " " << nextY << " " << nextX << endl;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+// Узел queue, используемый в BFS
+struct Node
+{
+    // (x, y) представляет координаты шахматной доски
+    // `dist` представляет минимальное расстояние от источника
+    int x, y, dist;
+
+    // Конструктор узла
+    Node(int x, int y, int dist = 0) : x(x), y(y), dist(dist) {}
+
+    // Поскольку мы используем структуру в качестве ключа в `std::set`,
+    // нам нужно перегрузить оператор `<`.
+    // В качестве альтернативы мы можем использовать `std::pair<int, int>` в качестве ключа
+    // для хранения координат матрицы в наборе.
+
+    bool operator<(const Node& o) const {
+        return x < o.x || (x == o.x && y < o.y);
+    }
+};
+
+// Находим минимальное количество шагов, которое сделал конь
+// из источника в пункт назначения с помощью BFS
+int findShortestDistance(int N, Node src, Node dest)
+{
+    // ставим проверять, была ли ячейка матрицы посещена раньше или нет
+    set<Node> visited;
+
+    // создаем queue и ставим в queue первый узел
+    queue<Node> q;
+    q.push(src);
+
+    // цикл до тех пор, пока queue не станет пустой
+    while (!q.empty())
+    {
+        // удалить передний узел из очереди и обработать его
+        Node node = q.front();
+        q.pop();
+
+        int x = node.x;
+        int y = node.y;
+        int dist = node.dist;
+
+        // если пункт назначения достигнут, возвращаем расстояние
+        if (x == dest.x && y == dest.y) {
+            return dist;
+        }
+
+        // пропустить, если место было посещено раньше
+        if (!visited.count(node))
+        {
+            // отметить текущий узел как посещенный
+            visited.insert(node);
+
+            // проверка всех восьми возможных движений коня
+            // и ставим в queue каждое допустимое движение
+            for (int i = 0; i < 12; i++)
+            {
+                // получаем действительную позицию коня из текущей позиции на
+                // шахматная доска и поставить ее в queue с расстоянием +1
+                int x1 = x + movesHorse[i][0];
+                int y1 = y + movesHorse[i][1];
+
+                if (isValid(x1, y1, N + movesHorse[i][2])) {
+                    q.push({ x1, y1, dist + 1 });
+                }
+            }
+        }
+    }
+
+    // возвращаем бесконечность, если путь невозможен
+    return INT_MAX;
+}
+
+
+
+
+
+
+
+
 void display(void)
 {
 
@@ -191,7 +307,7 @@ void display(void)
         in++;
     }
 
-    list[5].figure = 1;
+    list[8].figure = 1;
     list[24].figure = 2;
 
     for (int i = 0; i < list.size(); i++) {
@@ -224,25 +340,28 @@ void display(void)
         glVertex3f(list[i].x + mi / 2, list[i].y - mi / 2, 0);
         glEnd();
     }
+    cout << "\n\n\n";
+
+
+
+
+    int knightX = list[24].col, knightY = list[24].row; // координаты коня
+    int needX = list[0].col, needY = list[0].row;
+
 
     vector<Point> tmpList;
-    int knightX = list[24].col, knightY = list[24].row; // координаты коня
-    for (auto move1 : moves)
+
+    for (auto move1 : movesHorse)
     {
         int nextY = knightY + move1[1];
         int nextX = knightX;
-        if (((nextX == list[24].dist + move1[2] - 1) || nextX == 0) && (abs(move1[2]) == 2)) {
-            continue;
-        }
-
-
 
         nextX += move1[0];
         if (isValid(nextX, nextY, list[24].dist + move1[2])) {
             cout << "(" << nextX << "," << nextY << ")" << endl;
             for (int i = 0; i < list.size(); i++)
             {
-                if (list[i].row == nextY && list[i].col== nextX) {
+                if (list[i].row == nextY && list[i].col == nextX) {
                     renderText(list[i].x, list[i].y, "XYI");
                     tmpList.push_back(list[i]);
                     cout << list[i].index << " " << nextY << " " << nextX << endl;
@@ -251,13 +370,13 @@ void display(void)
             }
         }
     }
-    glColor3f(0.0, 0.0, 1.0);
-    for (int i = 0; i < tmpList.size(); i++) {
-        horse(tmpList[i], list);
-    }
 
-    cout << "\n\n\n";
-    cout << dist << "\n\n\n";
+        Node src = { knightX,  knightY };
+    Node dest = { needX, needY };
+    cout << "The minimum number of steps required is " <<
+        findShortestDistance(n, src, dest);
+
+
 
     glutSwapBuffers();
     glFlush();
