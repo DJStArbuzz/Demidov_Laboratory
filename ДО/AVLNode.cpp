f0627d07d73011a4413cc53a27e809b1b4e42e37
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-05-25 21:19:26 +0300

    Update AVLNode.cpp

--- ДО/AVLNode.cpp
+++ ДО/AVLNode.cpp
@@ -1,256 +1,217 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
 class AVLNode {
 public:
-    int key;
-    AVLNode* left;
-    AVLNode* right;
-    int depth;
+	int key;
+	AVLNode* left;
+	AVLNode* right;
+	int depth;
 };
 
-// Определяет какое поддерево больше
 int max(int a, int b) {
-    return (a > b) ? a : b;
+	return a > b ? a : b;
 }
 
-// Глубина
 int depth(AVLNode* n) {
-    // Левый сын, которого нет
-    if (n == NULL) {
-        return 0;
-    }
-    return n->depth;
+	if (n == NULL)
+		return 0;
+	return n->depth;
 }
 
-// Узел
 AVLNode* newNode(int key) {
-    AVLNode* node = new AVLNode();
-    node->key = key;
-    node->left = NULL;
-    node->right = NULL;
-    node->depth = 1;
-    return node;
+	AVLNode* node = new AVLNode();
+	node->key = key;
+	node->left = NULL;
+	node->right = NULL;
+	node->depth = 1;
+	return node;
 }
 
-// Поворот вправо поддерева с корнем y
+//повернуть вправо поддерево с корнем y
 AVLNode* rightRotate(AVLNode* y) {
-    // Эти 2 сына обязаны быть в реализации
-    AVLNode* x = y->left;
-    AVLNode* T2 = x->right;
-
-    // Вращение
-    x->right = y;
-    y->left = T2;
-
-    // Пересобираем высоты
-    y->depth = max(depth(y->left), depth(y->right)) + 1;
-    x->depth = max(depth(x->left), depth(x->right)) + 1;
-    return x;
+	AVLNode* x = y->left;
+	AVLNode* T2 = x->right;
+	//вращение
+	x->right = y;
+	y->left = T2;
+	y->depth = max(depth(y->left), depth(y->right)) + 1;
+	x->depth = max(depth(x->left), depth(x->right)) + 1;
+	return x;
 }
 
-// Поворот влево поддерева с корнем x
+//повернуть влево
 AVLNode* leftRotate(AVLNode* x) {
-    // Эти 2 сына обязаны быть в реализации
-    AVLNode* y = x->right;
-    AVLNode* T2 = y->left;
+	AVLNode* y = x->right;
+	AVLNode* T2 = y->left;
 
-    // Вращение
-    y->left = x;
-    x->right = T2;
-
-    // Пересобираем высоты
-    x->depth = max(depth(x->left), depth(x->right)) + 1;
-    y->depth = max(depth(y->left), depth(y->right)) + 1;
-    return y;
+	y->left = x;
+	x->right = T2;
+	x->depth = max(depth(x->left), depth(x->right)) + 1;
+	y->depth = max(depth(y->left), depth(y->right)) + 1;
+	return y;
 }
 
 int getBalance(AVLNode* N) {
-    if (N == NULL) {
-        return 0;
-    }
-
-    // Хотелось бы -1, 0, 1
-    // Если вылезаем за рамки, то все меняем
-    return depth(N->left) - depth(N->right);
+	if (N == NULL)
+		return 0;
+	return depth(N->left) - depth(N->right);
 }
 
-// Вставка элемента
+//вставка элементов
 AVLNode* insert(AVLNode* node, int key) {
-    if (node == NULL) {
-        return newNode(key);
-    }
-    if (key < node->key) {
-        node->left = insert(node->left, key);
-    }
-    else if (key > node->key ){
-        node->right = insert(node->right, key);
-    }
-    else {
-        return node;
-    }
-    node->depth = 1 + max(depth(node->left), depth(node->right));
+	if (node == NULL)
+		return newNode(key);
+	if (key < node->key)
+		node->left = insert(node->left, key);
+	else if (key > node->key)
+		node->right = insert(node->right, key);
+	else
+		return node;
+	node->depth = 1 + max(depth(node->left), depth(node->right));
+	int balace = getBalance(node);
 
-    int balance = getBalance(node);
-    // Поворот лево-лево
-    if (balance > 1 && key < node->left->key) {
-        return rightRotate(node); 
-    }
-    
-    // Поврот вправо вправо
-    if (balance < -1 && key > node->right->key) {
-        return leftRotate(node);
-    }
+	if (balace > 1 && key < node->left->key)
+		return rightRotate(node);
+	if (balace<-1 && key>node->right->key)
+		return leftRotate(node);
+	//лево право
+	if (balace > 1 && key > node->left->key) {
+		node->left = leftRotate(node->left);
+		return rightRotate(node);
+	}
+	//право лево
+	if (balace < -1 && key < node->right->key) {
+		node->right = rightRotate(node->right);
+		return leftRotate(node);
+	}
 
-    // Поворот лево право
-    if (balance > 1 && key > node->left->key) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-
-    // Поворот право влево
-    if (balance < -1 && key < node->right->key) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
-
-    return 0;
+	return node;
 }
 
-// Удаление узла
 AVLNode* minValueNode(AVLNode* node) {
-    AVLNode* current = node;
-    
-    while (current->left != NULL) {
-        current = current->left;
-    }
-
-    return current;
+	AVLNode* current = node;
+	while (current->left != NULL)
+	{
+		current = current->left;
+	}
+	return current;
 }
 
-AVLNode* deleteNode(AVLNode* root, int key) 
-{
-    if (root == NULL) {
-        return root;
-    }
+AVLNode* deleteNode(AVLNode* root, int key) {
+	if (root == NULL)
+		return root;
+	if (key < root->key)
+		root->left = deleteNode(root->left, key);
+	else if (key > root->key)
+		root->right = deleteNode(root->right, key);
+	else
+	{
+		if (root->left == NULL || root->right == NULL) {
+			AVLNode* temp = root->left ? root->left : root->right;
+			if (temp == NULL) {
+				temp = root;
+				root = NULL;
+			}
+			else
+				*root = *temp;
 
-    if (key < root->key) {
-        root->left = deleteNode(root->left, key);
-    }
-    else if (key > root->key) {
-        root->right = deleteNode(root->right, key);
-    }
-    else {
-        if ((root->left == NULL) || (root->right == NULL)) {
-            AVLNode* temp = root->left ? root->left : root->right;
-            if (temp == NULL) {
-                temp = root;
-                root = NULL;
-            }
-            else {
-                *root = *temp;
-            }
+			free(temp);
+		}
+		else
+		{
+			AVLNode* temp = minValueNode(root->right);
+			root->key = temp->key;
+			root->right = deleteNode(root->right, temp->key);
+		}
+	}
 
-            free(temp);
-        }
-        else {
-            // Заменили значение в текущем узле
-            AVLNode* temp = minValueNode(root->right);
-            root->key = temp->key;
-            root->right = deleteNode(root->right, temp->key); // Удаление может быть рекурсивно
-        }
-    }
+	if (root == NULL)
+		return root;
+	root->depth = 1 + max(depth(root->left), depth(root->right));
+	int balance = getBalance(root);
 
-    // Спустились до уровня листа
-    if (root == NULL) {
-        return root;
-    }
-
-    root->depth = 1 + max(depth(root->left), depth(root->right));
-    int balance = getBalance(root);
-
-    // Лево-лево
-    if (balance > 1 && getBalance(root->left) >= 0) {
-        return rightRotate(root);
-    }
-
-    // Лево-право
-    if (balance > 1 && getBalance(root->left) < 0) {
-        root->left = leftRotate(root->left);
-        return rightRotate(root);
-    }
-
-    // Право-право
-    if (balance < -1 && getBalance(root->right) <= 0) {
-        return leftRotate(root);
-    }
-
-    // Право-лево
-    if (balance < -1 && getBalance(root->right) > 0) {
-        root->right = rightRotate(root->right);
-        return leftRotate(root);
-    }
-    return root;
+	//лево лево
+	if (balance > 1 && getBalance(root->left) >= 0)
+		return rightRotate(root);
+	//лево право
+	if (balance > 1 && getBalance(root->left) < 0) {
+		root->left = leftRotate(root->left);
+		return rightRotate(root);
+	}
+	//право право
+	if (balance < -1 && getBalance(root->right) <= 0)
+		return leftRotate(root);
+	//право лево
+	if (balance < -1 && getBalance(root->right) > 0) {
+		root->right = rightRotate(root->right);
+		return leftRotate(root);
+	}
+	return root;
 }
 
 void inOrder(AVLNode* root) {
-    if (root != NULL) {
-        inOrder(root->left);
-        cout << root->key << " ";
-        inOrder(root->right);
-    }
+	if (root != NULL) {
+		inOrder(root->left);
+		cout << root->key << " ";
+		inOrder(root->right);
+	}
 }
 
 void show(AVLNode* Elem, int level) {
-    if (Elem->left != NULL) {
-        show(Elem->left, level + 1);
-    }
-
-    for (int i = 0; i < level; i++) {
-        cout << "  ";
-    }
-
-    cout << Elem->key << '\n';
-    if (Elem->right != NULL) {
-        show(Elem->right, level + 1);
-    }
+	if (Elem->left != NULL)
+		show(Elem->left, level + 1);
+	for (int i = 0; i < level; i++)
+		cout << "          ";
+	cout << Elem->key << "\n";
+	if (Elem->right != NULL)
+		show(Elem->right, level + 1);
 }
 
 int main()
 {
-    // AVL дерево
-    // баланс в дереве
-    /*
-        Примеры: баланс по количеству элементов
-                 баланс по глубине дерева (смотрим на количество связей с края слева и с края справа)
+	AVLNode* root = NULL;
+	root = insert(root, 12);
+	root = insert(root, 8);
+	root = insert(root, 45);
+	root = insert(root, 21);
+	root = insert(root, 67);
+	root = insert(root, 3);
+	root = insert(root, 9);
+	root = insert(root, 5);
+	root = insert(root, 3);
+	root = insert(root, 9);
+	root = insert(root, 56);
+	root = insert(root, 21);
+	root = insert(root, 29);
+	root = insert(root, 40);
+	root = insert(root, 72);
+	root = insert(root, 11);
+	root = insert(root, 12);
+	root = insert(root, 13);
+	root = insert(root, 14);
+	root = insert(root, 15);
+	root = insert(root, 16);
+	root = insert(root, 17);
+	root = insert(root, 18);
+	for (int i = 0;i < 100;i++)
+		root = insert(root, i);
 
-        Поворот - если мы удаляем или добавляем элементы, то поменяется баланс. Мы будем перемешивать узлы в дереве, чтобы получить балансировку
-
-        set и map - следствия AVL дерева
-    */
-
-    AVLNode* root = NULL;
-    root = insert(root, 12);
-    root = insert(root, 8);
-    root = insert(root, 18);
-    root = insert(root, 5);
-    root = insert(root, 11);
-    root = insert(root, 17);
-    root = insert(root, 4);
-    root = insert(root, 1);
-    root = insert(root, 2);
-    root = insert(root, 3);
-    root = insert(root, 6);
-    root = insert(root, 7);
-
-    show(root, 0);
-    root = deleteNode(root, 4);
-    show(root, 0);
-
-    return 0;
+	show(root, 0);
 }
+/*
+ ссылки:
+предок
+левый сын
+правый сын
 
+ функции:
+insert
+erase
+find
+
+хотим чтобы работали быстро (быстрее линейного)
+правило построения: (самый левый путь - самый правый) по модулю не больше 1.
+поворот для балансировки
+*/
